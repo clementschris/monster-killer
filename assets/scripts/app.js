506c7c1a1bd23ba5816222f4eed57223a5ec4758
Chris Clements <chrisclementss@gmail.com>
2021-01-30 12:17:36 -0500

    ternary operators added to attack function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -56,7 +56,7 @@ function endRound() {
     const initialPlayerHealth = currentPlayerHealth;
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerDamage;
-    
+
     writeToLog(
         LOG_EVENT_MOSTER_ATTACK,
         playerDamage,
@@ -107,24 +107,22 @@ function endRound() {
 }
 
 function attackMonster(mode) {
-    let maxDamage;
-    let logEvent;
-    if (mode === MODE_ATTACK) {
-        maxDamage = ATTACK_VALUE;
-        logEvent = LOG_EVENT_PLAYER_ATTACK;
-    } else if (mode === MODE_STRONG_ATTACK) {
-        maxDamage = STRONG_ATTACK_VALUE;
-        logEvent = LOG_EVENT_PLAYER_STRONG_ATTACK;
-    }
+    const maxDamage = mode === MODE_ATTACK ? ATTACK_VALUE : STRONG_ATTACK_VALUE;
+    const logEvent =
+        mode === MODE_ATTACK
+            ? LOG_EVENT_PLAYER_ATTACK
+            : LOG_EVENT_PLAYER_STRONG_ATTACK;
+    // if (mode === MODE_ATTACK) {
+    //     maxDamage = ATTACK_VALUE;
+    //     logEvent = LOG_EVENT_PLAYER_ATTACK;
+    // } else if (mode === MODE_STRONG_ATTACK) {
+    //     maxDamage = STRONG_ATTACK_VALUE;
+    //     logEvent = LOG_EVENT_PLAYER_STRONG_ATTACK;
+    // }
     const damage = dealMonsterDamage(maxDamage);
     currentMonsterHealth -= damage;
 
-    writeToLog(
-        logEvent,
-        damage,
-        currentMonsterHealth,
-        currentPlayerHealth
-    );
+    writeToLog(logEvent, damage, currentMonsterHealth, currentPlayerHealth);
 
     endRound();
 }
